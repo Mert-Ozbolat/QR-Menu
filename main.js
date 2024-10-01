479b6c5aa9b7c0de776d56f5187000a6298d91be
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-10-01 23:32:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+import { renderCards } from "./JS/ui.js";
+let data;
+
+async function fetchMenu() {
+    const res = await fetch("db.json");
+    data = await res.json();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetchMenu().then(() => renderCards(data.menu));
+});
+
+
+const inputs = document.querySelectorAll("#buttons input");
+inputs.forEach((input) => {
+    input.addEventListener("change", () => {
+        const selected = input.id;
+
+        if (selected === "all") {
+            renderCards(data.menu)
+        } else {
+            const filter = data.menu.filter((i) => i.category === selected);;
+            renderCards(filter);
+        }
+    });
+})
